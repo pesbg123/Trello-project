e182a51a8b176593debb8ce763be7a6b1c290eab
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-08 00:53:24 +0900

    [추가] jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './_common/orm.config';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({ useFactory: ormConfig }), UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({ useFactory: ormConfig }), UsersModule, JwtModule],
   controllers: [AppController],
   providers: [AppService],
 })
