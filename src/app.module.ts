3bac334ab2330c7f95c4502485d23bb3bfb2b627
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-07 12:17:10 +0900

    [add] TypeORmModule usefactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './_common/orm.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({ useFactory: ormConfig })],
   controllers: [AppController],
   providers: [AppService],
 })
