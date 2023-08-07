3cb51ef6a6c0e342b4948bb0db8e8afbaf8bbdb6
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-07 20:26:23 +0900

    [add] users Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './_common/orm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({ useFactory: ormConfig })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({ useFactory: ormConfig }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
