bf9c33eda55428b57de8b31e3ffe027b203a8e8f
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-08 14:20:29 +0900

    add

--- src/_common/utils/password.hash.ts
+++ src/_common/utils/password.hash.ts
@@ -1,7 +1,6 @@
 import * as bcrypt from 'bcrypt';
-import { SignupDto } from '../dtos/signup.dto';
 
-export const transfomrPassword = async (body: SignupDto): Promise<void> => {
+export const transfomrPassword = async (body: any): Promise<void> => {
   body.password = await bcrypt.hash(body.password, 10);
   return Promise.resolve();
 };
