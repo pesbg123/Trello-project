b2e0a2500f5f1d2ee2155330e72d66a5a703c0df
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-08 14:21:28 +0900

    auth gurard 로직 수정

--- src/_common/security/access.auth.guard.ts
+++ src/_common/security/access.auth.guard.ts
@@ -1,15 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Request } from 'express';
 import { Observable } from 'rxjs';
 import { JwtService } from 'src/jwt/jwt.service';
 import { UsersService } from 'src/users/users.service';
+import { IRequest } from '../interfaces/request.interface';
 
 @Injectable()
 export class accessAuthGuard implements CanActivate {
   constructor(private usersService: UsersService, private jwtService: JwtService) {}
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const request: Request = context.switchToHttp().getRequest();
-    /** return boolean 형식이며, true일 경우 가드 통과, 아닌경우 403 인가실패 */
+    const request: IRequest = context.switchToHttp().getRequest();
     return this.validate(request);
   }
 
@@ -22,6 +21,10 @@ export class accessAuthGuard implements CanActivate {
     /** 사용자 유효성 검증 및 DB에 등록된 토큰과 일치여부 검증 */
     const findByUser = await this.usersService.tokenValidateUser(payload);
     if (!findByUser || findByUser.token !== requestAccessToken) throw new UnauthorizedException();
+
+    //무조건 한번은 DB에 접근을 해야하기때문에 비용적인면에서 불리한편으로 캐시를 사용할 수 있도록 레디스를 사용할 예정
+
+    request.user = findByUser;
 
     return true;
   }
