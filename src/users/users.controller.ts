90964dd9e7755360d65023b4fd61a27e8e0dc576
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-08 14:21:16 +0900

    add get, patch, patch('password')

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { IMessage } from 'src/_common/interfaces/message.interface';
 import { UsersService } from './users.service';
 import { SignupDto } from 'src/_common/dtos/signup.dto';
@@ -8,14 +8,17 @@ import { Request, Response } from 'express';
 import { IToken } from 'src/_common/interfaces/Token.interface';
 import { IRequest } from 'src/_common/interfaces/request.interface';
 import { accessAuthGuard } from 'src/_common/security/access.auth.guard';
+import { User } from 'src/_common/entities/user.entity';
+import { EditProfileDto } from 'src/_common/dtos/editProfile.dto';
+import { EditPasswordDto } from 'src/_common/dtos/editPassword.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('signup')
-  async signup(@Body() body: SignupDto): Promise<IMessage> {
-    /** 프로필 사진 추가, body값 중에 password, confirmPassword 일치여부 체크 Pipe 필요  */
+  async signup(@Body() body: SignupDto): Promise<IResult> {
+    /** 프로필 사진 추가 필요  */
     return await this.usersService.signup(body);
   }
 
@@ -27,14 +30,31 @@ export class UsersController {
   }
 
   @Delete('logout')
+  @UseGuards(accessAuthGuard)
   async logout(@Req() req: IRequest): Promise<IResult> {
     const { id } = req.user;
     return await this.usersService.logout(id);
   }
 
-  @Get('test')
+  @Get()
   @UseGuards(accessAuthGuard)
-  test() {
-    return 'test';
+  async profile(@Req() req: IRequest): Promise<User> {
+    const { id } = req.user;
+    return await this.usersService.profile(id);
+  }
+
+  @Patch()
+  @UseGuards(accessAuthGuard)
+  async editProfile(@Req() req: IRequest, @Body() editData: EditProfileDto): Promise<IResult> {
+    /** 프로필 사진 추가 필요  */
+    const { id } = req.user;
+    return await this.usersService.editProfile(id, editData);
+  }
+
+  @Patch('password')
+  @UseGuards(accessAuthGuard)
+  async editPassword(@Req() req: IRequest, @Body() editPassword: EditPasswordDto): Promise<IResult> {
+    const user = req.user;
+    return await this.usersService.editPassword(user, editPassword);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,14 @@
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EditPasswordDto } from 'src/_common/dtos/editPassword.dto';
+import { EditProfileDto } from 'src/_common/dtos/editProfile.dto';
 import { LoginDto } from 'src/_common/dtos/login.dto';
 import { SignupDto } from 'src/_common/dtos/signup.dto';
 import { User } from 'src/_common/entities/user.entity';
 import { IToken } from 'src/_common/interfaces/Token.interface';
 import { IMessage } from 'src/_common/interfaces/message.interface';
 import { Payload } from 'src/_common/interfaces/payload.interface';
+import { IRequest } from 'src/_common/interfaces/request.interface';
 import { IResult } from 'src/_common/interfaces/result.interface';
 import { validatePassword } from 'src/_common/utils/password.compare';
 import { transfomrPassword } from 'src/_common/utils/password.hash';
@@ -18,12 +21,12 @@ export class UsersService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>, private jwtService: JwtService) {}
 
   /** 회원가입 */
-  async signup(body: SignupDto): Promise<IMessage> {
+  async signup(body: SignupDto): Promise<IResult> {
     const emailExists = await this.userRepository.findOne({ where: { email: body.email } });
     if (emailExists) throw new HttpException('이미 존재하는 이메일입니다.', HttpStatus.BAD_REQUEST);
     await transfomrPassword(body);
     await this.userRepository.save(body);
-    return { message: '회원가입이 완료되었습니다.' };
+    return { result: true };
   }
 
   /** 로그인 */
@@ -31,10 +34,16 @@ export class UsersService {
     const findByUser = await this.userRepository.findOne({ where: { email: body.email } });
     const validPassword = await validatePassword(findByUser.password, body.password);
 
+    const remainingToken = findByUser.token;
+
+    if (remainingToken) {
+      const verifyErrorHandle = this.jwtService.verifyErrorHandle(remainingToken);
+      if (verifyErrorHandle !== 'jwt expired') throw new UnauthorizedException('이미 로그인된 계정입니다.');
+    }
+
     if (!findByUser || !validPassword) throw new UnauthorizedException('이메일과 패스워드를 확인해 주세요.');
 
     const accessToken = this.jwtService.sign({ id: findByUser.id }, process.env.JWT_ACCESS_EXPIRATION_TIME);
-
     await this.userRepository.update({ id: findByUser.id }, { token: accessToken });
 
     return {
@@ -48,8 +57,27 @@ export class UsersService {
     return { result: true };
   }
 
-  //** 토큰 값 실제 사용자 유효성 검증 */
+  /** 토큰 값 실제 사용자 유효성 검증 */
   async tokenValidateUser(payload: any): Promise<User> {
     return await this.userRepository.findOne({ where: { id: payload.id } });
+  }
+
+  /** 사용자 정보 조회 */
+  async profile(id: number): Promise<User> {
+    return await this.userRepository.findOne({ where: { id }, select: { email: true, name: true, createdAt: true } });
+  }
+
+  /** 사용자 정보 수정 */
+  async editProfile(id: number, editData: EditProfileDto): Promise<IResult> {
+    await this.userRepository.update({ id }, editData);
+    return { result: true };
+  }
+
+  /** 사용자 패스워드 수정 */
+  async editPassword(user: User, editPassword: EditPasswordDto): Promise<IResult> {
+    if (user.password == editPassword.password) throw new BadRequestException('현재 사용중인 패스워드입니다.');
+    await transfomrPassword(editPassword);
+    await this.userRepository.update({ id: user.id }, { password: editPassword.password });
+    return { result: true };
   }
 }
