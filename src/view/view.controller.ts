6dccf5f03267c73d58a57a8a5670aed1cadef248
inhanbyeol <inhanbyeol94@gmail.com>
2023-08-09 20:55:33 +0900

    프론트 작업 (상세설명 생략)

--- src/view/view.controller.ts
+++ src/view/view.controller.ts
@@ -1,10 +1,43 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req, UseGuards } from '@nestjs/common';
+import { ViewService } from './view.service';
+import { Request } from 'express';
+import { IAccessPayload } from 'src/_common/interfaces/access.payload.interface';
+import { IsLogin } from 'src/_common/security/isLogin.guard';
+import { IRequest } from 'src/_common/interfaces/request.interface';
 
 @Controller()
 export class ViewController {
+  constructor(private viewService: ViewService) {}
+
+  /** headers */
   @Get()
+  @UseGuards(IsLogin)
   @Render('index.ejs')
-  async test() {
-    return { name: '심재두' };
+  async index(@Req() req: IRequest) {
+    const user: IAccessPayload = req.user;
+    const header = await this.viewService.header(user);
+    return { title: 'IDLE', subtitle: '대시보드', header };
+  }
+
+  @Get('myprofile')
+  @UseGuards(IsLogin)
+  @Render('myprofile.ejs')
+  async myProfile(@Req() req: IRequest) {
+    const user: IAccessPayload = req.user;
+    const header = await this.viewService.header(user);
+    return { title: 'IDLE', subtitle: '프로필', header };
+  }
+
+  /** No headers */
+  @Get('login')
+  @Render('login.ejs')
+  async login() {
+    return { title: 'IDLE', subtitle: '로그인' };
+  }
+
+  @Get('signup')
+  @Render('signup.ejs')
+  async signup() {
+    return { title: 'IDLE', subtitle: '회원가입' };
   }
 }
--- src/view/view.service.ts
+++ src/view/view.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { IAccessPayload } from 'src/_common/interfaces/access.payload.interface';
 
 @Injectable()
-export class ViewService {}
+export class ViewService {
+  async header(user: IAccessPayload) {
+    if (user) {
+      return {
+        isLogin: true,
+        email: user.email,
+        name: user.name,
+        imageUrl: user.imageUrl || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+      };
+    }
+
+    return {
+      isLogin: false,
+    };
+  }
+}
