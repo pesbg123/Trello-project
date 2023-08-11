0cdfb9adaad3d8d623ee3e8284b5e9c77cd7bf31
marshmello <pesbg123@gmail.com>
2023-08-11 11:21:39 +0900

    fix: commentModule에 MiddlewareConsumer연결

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,10 +11,15 @@ import { JwtService } from 'src/jwt/jwt.service';
 import { AccessAuthGuard } from 'src/_common/middlewares/security/access.auth.guard';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/_common/entities/user.entity';
+import { TokenValidMiddleware } from 'src/_common/middlewares/token.valid.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Comment, User]), ProjectsModule, BoardsModule],
   controllers: [CommentsController],
   providers: [CommentsService, JwtService, AccessAuthGuard, UsersService],
 })
-export class CommentsModule {}
+export class CommentsModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TokenValidMiddleware).forRoutes(CommentsController);
+  }
+}
